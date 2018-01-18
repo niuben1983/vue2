4ceb910a36dcfb60538d45fc3f9c5978457b5e97
牛奔 <niuben@intv.com.cn>
2018-01-18 21:33:41 +0800

    changed

--- webpack.config.js
+++ webpack.config.js
@@ -18,33 +18,13 @@ var entries = getEntry('./src/page/**/*.js'); // 获得入口 js 文件
 var chunks = Object.keys(entries)
 
 var CONFIG_DEV = require('./config/dev');
-var CONFIG_PRE = require('./config/pre');
-var CONFIG_PROD = require('./config/prod');
-var ENV = process.env.NODE_ENV;
-
-if (ENV === 'pre') {
-    var config = {
-        path: CONFIG_PRE.PATH,
-        publicPath: CONFIG_PRE.PUBLICPATH
-    };
-} else if (ENV === 'production') {
-    var config = {
-        path: CONFIG_PROD.PATH,
-        publicPath: CONFIG_PROD.PUBLICPATH
-    };
-} else {
-    var config = {
-        path: CONFIG_DEV.PATH,
-        publicPath: CONFIG_DEV.PUBLICPATH
-    };
-}
 
 module.exports = {
     /* 输入文件 */
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, config.path), // html, css, js 图片等资源文件的输出路径，将所有资源文件放在 dist 目录
-        publicPath: config.publicPath, // html, css, js 图片等资源文件的 server 上的路径
+        publicPath: 'dev', // html, css, js 图片等资源文件的 server 上的路径
         filename: 'js/[name].js', // 每个入口 js 文件的生成配置
         chunkFilename: 'js/[id].js'
     },
@@ -106,7 +86,6 @@ module.exports = {
             chunks: chunks, // chunks 是需要提取的模块
             // minChunks: chunks.length
         }),
-        new ExtractTextPlugin('css/[name].css'),
         new CopyWebpackPlugin([
             {
                 from: path.resolve(__dirname, 'src/lib/flexible'),
@@ -116,66 +95,15 @@ module.exports = {
                 ignore: ['.*']
             }
         ]),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, 'src/img'),
-                to: path.resolve(__dirname, config.path + '/img'),
-                force: true,
-                toType: 'dir',
-                ignore: ['.*']
-            }
-        ]),
-        // new CleanWebpackPlugin(
-        //   ['dist/js/*.js'], 　 //匹配删除的文件
-        //   {
-        //     root: __dirname,
-        //     //根目录
-        //     verbose: true,
-        //     //开启在控制台输出信息
-        //     dry: false　　　　　　　　　　 //启用删除文件
-        //   }
-        // )
-    ]
-}
-
-
-module.exports.plugins = (module.exports.plugins || []);
-if (ENV === 'production') {
-    //生产环境
-    module.exports.devtool = 'source-map';
-    module.exports.plugins = module.exports.plugins.concat([
-        // 借鉴 vue 官方的生成环境配置
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            },
-            'CONFIG': JSON.stringify(CONFIG_PROD)
-        }),
-
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ]);
-} else if (ENV === 'pre') {
-    //测试环境
-    module.exports.devtool = 'source-map';
-    module.exports.plugins = module.exports.plugins.concat([
-        // 借鉴 vue 官方的生成环境配置
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"prerelease"'
-            },
-            'CONFIG': JSON.stringify(CONFIG_PRE)
-        }),
-
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ]);
-} else {
-    // dev环境
-    module.exports.devtool = 'eval-source-map';
-    module.exports.plugins = module.exports.plugins.concat([
+        // new CopyWebpackPlugin([
+        //     {
+        //         from: path.resolve(__dirname, 'src/img'),
+        //         to: path.resolve(__dirname, config.path + '/img'),
+        //         force: true,
+        //         toType: 'dir',
+        //         ignore: ['.*']
+        //     }
+        // ]),
         new CopyWebpackPlugin([
             {
                 from: path.resolve(__dirname, 'src/api'),
@@ -192,47 +120,21 @@ if (ENV === 'production') {
             },
             'CONFIG': JSON.stringify(CONFIG_DEV)
         }),
-
-    ]);
+        // new CleanWebpackPlugin(
+        //   ['dist/js/*.js'], 　 //匹配删除的文件
+        //   {
+        //     root: __dirname,
+        //     //根目录
+        //     verbose: true,
+        //     //开启在控制台输出信息
+        //     dry: false　　　　　　　　　　 //启用删除文件
+        //   }
+        // )
+    ],
+    devtool: 'eval-source-map'
 }
 
 
-var pages = getEntry('./src/html/*.html');
-for (var pathname in pages) {
-    // 配置生成的 html 文件，定义路径等
 
-    var conf = {
-        filename: pathname + '.html', // html 文件输出路径
-        template: pages[pathname], // 模板路径
-        inject: 'body', // js 插入位置
-        minify: {
-            removeComments: true,
-            collapseWhitespace: false
-        },
-        files: {
-            "flexible": config.publicPath + "lib/flexible/"
-        }
-    };
-    if (pathname in module.exports.entry) {
-        conf.chunks = ['vendors', pathname];
-        conf.hash = false;
-    }
-    // 需要生成几个 html 文件，就配置几个 HtmlWebpackPlugin 对象
-    module.exports.plugins.push(new HtmlWebpackPlugin(conf));
-}
 
-// 根据项目具体需求，具体可以看上面的项目目录，输出正确的 js 和 html 路径
-// 根据项目具体需求，输出正确的 js 和 html 路径
-function getEntry(globPath) {
-    var entries = {},
-        basename, tmp, pathname;
 
-    glob.sync(globPath).forEach(function (entry) {
-        basename = path.basename(entry, path.extname(entry));
-        tmp = entry.split('/').splice(-3);
-        pathname = basename; // 正确输出 js 和 html 的路径
-        entries[pathname] = entry;
-    });
-    console.log(entries);
-    return entries;
-}
