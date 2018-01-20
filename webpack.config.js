94280c08def754b5d9a6423174e146bc55a4974d
牛奔 <niuben@intv.com.cn>
2018-01-20 16:20:22 +0800

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ require('require-yaml');
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var glob = require('glob');
 
 // 将样式提取到单独的 css 文件中，而不是打包到 js 文件或使用 style 标签插入在 head 标签中
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -14,16 +13,13 @@ var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var entries = getEntry('./src/page/**/*.js'); // 获得入口 js 文件
-var chunks = Object.keys(entries)
-
 var CONFIG_DEV = require('./config/dev');
 
 module.exports = {
     /* 输入文件 */
     entry: './src/main.js',
     output: {
-        path: path.resolve(__dirname, config.path), // html, css, js 图片等资源文件的输出路径，将所有资源文件放在 dist 目录
+        path: path.resolve(__dirname, 'dev'), // html, css, js 图片等资源文件的输出路径，将所有资源文件放在 dist 目录
         publicPath: 'dev', // html, css, js 图片等资源文件的 server 上的路径
         filename: 'js/[name].js', // 每个入口 js 文件的生成配置
         chunkFilename: 'js/[id].js'
@@ -36,65 +32,80 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: ['css-loader', 'postcss-loader']
-            })
-        },
-        /* 用来解析vue后缀的文件 */
-        {
-            test: /\.vue$/,
-            loader: 'vue-loader',
-            exclude: /node_modules/,
-            options: {
-                // postcss: [require('autoprefixer')({
-                //     browsers: ['last 2 versions']
-                // })],
-                // loaders: {
-                //     css: ExtractTextPlugin.extract({
-                //         use: ['css-loader'],
-                //         fallback: 'vue-style-loader'
-                //     })
-                //     //....scss less sass more
-                // },
-                // sourceMap: true
+                test: /\.css$/,
+                loaders: [{
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'style-loader'
+                    }
+                ]
+            },
+            /* 用来解析vue后缀的文件 */
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                exclude: /node_modules/,
+                options: {
+                    // postcss: [require('autoprefixer')({
+                    //     browsers: ['last 2 versions']
+                    // })],
+                    // loaders: {
+                    //     css: ExtractTextPlugin.extract({
+                    //         use: ['css-loader'],
+                    //         fallback: 'vue-style-loader'
+                    //     })
+                    //     //....scss less sass more
+                    // },
+                    // sourceMap: true
+                }
+            },
+            /* 用babel来解析js文件并把es6的语法转换成浏览器认识的语法 */
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                /* 排除模块安装目录的文件 */
+                exclude: [/node_modules/]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                // 图片加载器，较小的图片转成 base64
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: './img/[name].[ext]?[hash:7]'
+                }
             }
-        },
-        /* 用babel来解析js文件并把es6的语法转换成浏览器认识的语法 */
-        {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            /* 排除模块安装目录的文件 */
-            exclude: [/node_modules/]
-        },
-        {
-            test: /\.(png|jpe?g|gif|svg)$/,
-            // 图片加载器，较小的图片转成 base64
-            loader: 'url-loader',
-            query: {
-                limit: 10000,
-                name: './img/[name].[ext]?[hash:7]'
-            }
-        }
         ]
     },
     plugins: [
         // 提取公共模块
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendors', // 公共模块的名称
-            chunks: chunks, // chunks 是需要提取的模块
+            // chunks: chunks, // chunks 是需要提取的模块
             // minChunks: chunks.length
         }),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, 'src/lib/flexible'),
-                to: path.resolve(__dirname, config.path + '/lib/flexible'),
-                force: true,
-                toType: 'dir',
-                ignore: ['.*']
+        new HtmlWebpackPlugin({
+            filename: './index.html', // html 文件输出路径
+            template: './src/index.html', // 模板路径
+            inject: 'body', // js 插入位置
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false
             }
-        ]),
+        }),
+        // new CopyWebpackPlugin([
+        //     {
+        //         from: path.resolve(__dirname, 'src/lib/flexible'),
+        //         to: path.resolve(__dirname, config.path + '/lib/flexible'),
+        //         force: true,
+        //         toType: 'dir',
+        //         ignore: ['.*']
+        //     }
+        // ]),
         // new CopyWebpackPlugin([
         //     {
         //         from: path.resolve(__dirname, 'src/img'),
@@ -104,15 +115,15 @@ module.exports = {
         //         ignore: ['.*']
         //     }
         // ]),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, 'src/api'),
-                to: path.resolve(__dirname, config.path + '/api'),
-                force: true,
-                toType: 'dir',
-                ignore: ['.*']
-            }
-        ]),
+        // new CopyWebpackPlugin([
+        //     {
+        //         from: path.resolve(__dirname, 'src/api'),
+        //         to: path.resolve(__dirname, config.path + '/api'),
+        //         force: true,
+        //         toType: 'dir',
+        //         ignore: ['.*']
+        //     }
+        // ]),
         // 借鉴 vue 官方的生成环境配置
         new webpack.DefinePlugin({
             'process.env': {
@@ -133,8 +144,3 @@ module.exports = {
     ],
     devtool: 'eval-source-map'
 }
-
-
-
-
-
