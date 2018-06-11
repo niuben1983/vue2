498d5055d0b9ae8ddb4d1af2145312177fd78666
niuben <bniu2006@qq.com>
2018-06-11 11:07:09 +0800

    '不打包指定的js'

---
+++ src/config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by niub on 2018/6/11.
+ */
+
+const base = {
+    webpath : 'http://192.168.0.1'
+}
--- src/page/index/app.vue
+++ src/page/index/app.vue
@@ -36,6 +36,7 @@
         created() {
         },
         mounted() {
+            console.log(base.webpath);
         }
     };
 </script>
